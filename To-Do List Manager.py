85ace1aa92dbd163f0435660d6854054ccad1918
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-06-10 16:48:17 +0330

    a while loop added
    It displays 5 options (Add Task, View Tasks, Mark Task as Completed, Remove, Exit) and continues indefinitely until the user chooses to exit.

--- To-Do List Manager.py
+++ To-Do List Manager.py
@@ -2,6 +2,8 @@
 def main():
     tasks = []
     print("Welcome to the To-Do List Manager!")
+    while True:
+        print("\n1. Add Task\n2. View Tasks\n3. Mark Task as Completed\n4. Remove Task\n5. Exit")
    
 if __name__ == "__main__":
     main()
